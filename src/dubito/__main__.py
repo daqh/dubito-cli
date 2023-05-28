9f1041cf150172a2fbe3455452a9ccdbaa7473fc
daqh <valeriodipasq@gmail.com>
2023-05-28 23:18:43 +0200

    Updated subito_page_list

--- src/dubito/__main__.py
+++ src/dubito/__main__.py
@@ -3,10 +3,12 @@ import requests_cache
 from datetime import timedelta
 import logging
 import argparse
-from dubito.subito_list_page.iterators import subito_list_page_item_list_from_query
+from dubito.subito_list_page import subito_list_page_item_iterator, SubitoListPageQuery
 import csv
+import sys
 
 def main():
+
     parser = argparse.ArgumentParser(
         prog='dubito',
         description='Get the insertions from a query.',
@@ -14,20 +16,20 @@ def main():
     )
 
     parser.add_argument('query', type=str, help='The query to search.')
-    parser.add_argument('-i', '--include', type=str, nargs="+", help='The query to search.', default=[])
-    parser.add_argument('-e', '--exclude', type=str, nargs="+", help='The query to search.', default=[])
-    parser.add_argument('--minimum-price', type=int, help='The minimum price.', default=0)
-    parser.add_argument('--maximum-price', type=int, help='The maximum price.')
+    # parser.add_argument('-i', '--include', type=str, nargs="+", help='The query to search.', default=[])
+    # parser.add_argument('-e', '--exclude', type=str, nargs="+", help='The query to search.', default=[])
+    # parser.add_argument('--minimum-price', type=int, help='The minimum price.', default=0)
+    # parser.add_argument('--maximum-price', type=int, help='The maximum price.')
     parser.add_argument('--install-cache', action='store_true', help='Install the cache.', default=False)
     parser.add_argument('-v', '--verbose', action='store_true', help='Verbose.', default=False)
-    parser.add_argument('-o', '--output', type=str, help='The output file.', default="csv")
+    # parser.add_argument('-o', '--output', type=str, help='The output file.', default="csv")
     args = parser.parse_args()
 
     query = args.query
-    include = args.include
-    exclude = args.exclude
-    minimum_price = args.minimum_price
-    maximum_price = args.maximum_price
+    # include = args.include
+    # exclude = args.exclude
+    # minimum_price = args.minimum_price
+    # maximum_price = args.maximum_price
     install_cache = args.install_cache
     verbose = args.verbose
 
@@ -38,7 +40,9 @@ def main():
         logging.info("Installing cache")
         requests_cache.install_cache('dubito_cache', backend="sqlite", expire_after=timedelta(hours=1))
 
-    insertions = subito_list_page_item_list_from_query(query)
+    # insertions = subito_list_page_item_list_from_query(query)
+
+    subito_list_page_items = list(subito_list_page_item_iterator(SubitoListPageQuery(query)))
 
     # if minimum_price:
     #     insertions = insertions[insertions["price"] >= minimum_price]
@@ -64,6 +68,17 @@ def main():
     # iqr = q3 - q1
     # r = 1.5
     # insertions = insertions[~((insertions['price'] < (q1 - r * iqr)) | (insertions['price'] > (q3 + r * iqr)))]
+
+    # Covert to csv using csv module and print to console
+    # Don't use pandas to_csv method
+    # print(insertions.to_csv(index=False))
+
+    # insertions.to_csv("output.csv", index=False)
+
+    writer = csv.writer(sys.stdout, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    # writer.writerow()
+    for item in subito_list_page_items:
+        writer.writerow(item.values())
 
 if __name__ == "__main__":
     main()
--- src/dubito/subito_list_page/__init__.py
+++ src/dubito/subito_list_page/__init__.py
@@ -1,302 +1,161 @@
-import urllib.parse
-from datetime import datetime
+from pyparsing import Iterator
 from selectorlib import Extractor
 from dubito.utils import simplified_get, extractors_directory
-import logging
+from urllib.parse import urlparse, parse_qs
+from datetime import datetime
 
 class SubitoListPage:
-    '''
-    # Subito List Page
-    A class to represent a list page from subito.it.
 
-    Attributes
-    ----------
-    `url: str`
-        The url of the page.
-    `query: str`
-        The query of the page.
-    `page_number: int`
-        The number of the page.
-    '''
-
-    def __init__(self, url):
-        '''
-        # Subito List Page > Constructor
-        The constructor for the SubitoListPage class.
-
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
-
-        Raises
-        ------
-        `ValueError`
-            If the url does not contain a query.
-        '''
+    def __init__(self, url) -> None:
         self.__url = url
-        parsed_url = urllib.parse.urlparse(url)
-        parsed_qs = urllib.parse.parse_qs(parsed_url.query)
+        parsed_url = urlparse(url)
+        parsed_qs = parse_qs(parsed_url.query)
         try:
             self.__query = parsed_qs["q"][0]
         except:
             raise ValueError("The url must contain a query.")
-        self.__page_number = int(parsed_qs["o"][0]) if "o" in parsed_qs.keys() else 1
-    
+        page_number = int(parsed_qs["o"][0] if "o" in parsed_qs.keys() else 1)
+        if page_number < 1:
+            raise ValueError("The page number must be greater than 0.")
+        self.__page_number = int(page_number)
+
     @property
-    def url(self):
+    def url(self) -> str:
         return self.__url
     
     @property
-    def query(self):
+    def query(self) -> str:
         return self.__query
-
+    
     @property
-    def page_number(self):
+    def page_number(self) -> int:
         return self.__page_number
     
-    def __getitem__(self, key):
-        '''
-        # Subito List Page > Get Item
-        Get the page with the given number.
-
-        Example
-        -------
-        ```python
-        from subito import SubitoListPage
-
-        page = SubitoListPage("https://www.subito.it/annunci-italia/vendita/usato/?q=rtx%202080&o=1")
-        print(page[3].url)
-        # Output:
-        # https://www.subito.it/annunci-italia/vendita/usato/?q=rtx%202080&o=3
-        ```
-        '''
-        return SubitoQueryListPage(self.query, key)
+    def __iter__(self) -> "SubitoListPage":
+        subito_list_page = self
+        while True:
+            yield subito_list_page
+            try:
+                subito_list_page = subito_list_page[subito_list_page.page_number + 1]
+            except StopIteration:
+                break
+    
+    def __getitem__(self, page_number: int) -> "SubitoListPage":
+        return SubitoListPageQuery(self.query, page_number)
 
     def __str__(self) -> str:
-        return f"({self.__class__.__name__}: {self.query}, {self.page_number})"
+        return f"({self.__class__.__name__}: {self.url})"
 
-class SubitoQueryListPage(SubitoListPage):
-    '''
-    # Subito Query List Page
-    A class to represent a page from subito.it, with a query.
-    
-    By default this class produces a url in the form of:
-    `https://www.subito.it/annunci-italia/vendita/usato/?q={query}&o={page_number}`
-
-    ## Attributes
-    `url: str`
-        The url of the page.
-    `number: int = 1`
-        The number of the page.
-
-    ## Example
-    ```python
-    from subito import SubitoQueryListPage
-
-    page = SubitoQueryListPage("rtx 2080")
-    print(page.url)
-    ```
-    '''
+class SubitoListPageQuery(SubitoListPage):
 
     __url = "https://www.subito.it/annunci-italia/vendita/usato/?q={query}&o={page_number}"
 
-    def __init__(self, query: str, page_number: int = 1):
+    def __init__(self, query: str, page_number: int = 1) -> None:
         url = self.__url.format(query=query, page_number=page_number)
-        # Format the url
-        url = urllib.parse.quote(url, safe=':/?&=')
         super().__init__(url)
 
-class ExtractedSubitoListPage:
-    '''
-    # Extracted Subito List Page
-    A class to represent a list page from subito.it, with the text extracted.
+class ExtractedSubitoListPage(SubitoListPage):
 
-    ## Attributes
-    `url: str`
-        The url of the page.
-    `text: str`
-        The text of the page.
-    '''
-
-    def __init__(self, subito_list_page: SubitoListPage):
-        '''
-        # Extracted Subito List Page > Constructor
-        The constructor for the ExtractedSubitoListPage class.
-
-        Arguments
-        ---------
-        `subito_list_page: SubitoListPage`
-            The SubitoListPage object to extract.
-        '''
-        self.__subito_list_page = subito_list_page
-        logging.info(f"Extracting {subito_list_page}")
-        response_text = simplified_get(subito_list_page.url)
-        self.__response = response_text
+    def __init__(self, subito_list_page: SubitoListPage, response_text: str) -> None:
+        super().__init__(subito_list_page.url)
+        self.__response_text = response_text
 
     @property
-    def response(self):
-        return self.__response
+    def response_text(self) -> str:
+        return self.__response_text
     
-    @property
-    def subito_list_page(self):
-        return self.__subito_list_page
+    def __getitem__(self, page_number: int) -> SubitoListPage:
+        subito_list_page = super().__getitem__(page_number)
+        extracted_subito_list_page = extract_subito_list_page(subito_list_page)
+        return extracted_subito_list_page
 
-    def __str__(self) -> str:
-        return f"({self.__class__.__name__}: {self.subito_list_page})"
-    
-    @classmethod
-    def from_url(cls, url: str):
-        '''
-        # Extracted Subito List Page > From URL
-        Create an ExtractedSubitoListPage object from an url.
+class TransformedSubitoListPage(ExtractedSubitoListPage):
 
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
-
-        Example
-        -------
-        ```python
-        from subito import ExtractedSubitoListPage
-
-        page = ExtractedSubitoListPage.from_url("https://www.subito.it/annunci-italia/vendita/usato/?q=rtx%202080&o=1")
-        ```
-        '''
-        return cls(SubitoListPage(url))
-
-    @classmethod
-    def from_query(cls, query: str, page_number: int = 1):
-        '''
-        # Extracted Subito List Page > From Query
-        Create an ExtractedSubitoListPage object from a query.
-
-        Arguments
-        ---------
-        `query: str`
-            The query of the page.
-
-        Example
-        -------
-        ```python
-        from subito import ExtractedSubitoListPage
-
-        page = ExtractedSubitoListPage.from_query("rtx 2080")
-        ```
-        '''
-        return cls(SubitoQueryListPage(query, page_number))
-
-class TransformedSubitoListPage:
-    '''
-    # Transformed Subito List Page
-    A class to represent a list page from subito.it, with the text extracted and transformed.
-
-    Attributes
-    ----------
-    `subito_list_page_items: List[SubitoListItem]`
-        The list of items in the page.
-    `insertions: List[SubitoListItem]`
-        The list of items that are new in the page.
-
-    Example
-    -------
-    ```python
-    from subito import SubitoListPage, TransformedSubitoListPage
-
-    page = SubitoListPage("rtx 2080")
-    extracted_page = ExtractedSubitoListPage(page)
-
-    transformed_page = TransformedSubitoListPage(extracted_page)
-    print(transformed_page.insertions)
-    ```
-    '''
-
-    __subito_list_page_extractor = Extractor.from_yaml_file(f'{extractors_directory}/subito_list_page_extractor.yaml')
-
-    def __init__(self, extracted_subito_list_page: ExtractedSubitoListPage):
-        '''
-        # Transformed Subito List Page > Constructor
-
-        Arguments
-        ---------
-        `extracted_subito_list_page: ExtractedSubitoListPage`
-            The ExtractedSubitoListPage object to transform.
-        '''
-        self.__extracted_subito_list_page = extracted_subito_list_page
-        logging.info(f"Transforming {self}")
-        result = self.__subito_list_page_extractor.extract(extracted_subito_list_page.response)
-        self.__subito_list_page_items = result["subito_list_page_items"]
-        if not self.__subito_list_page_items:
-            self.__subito_list_page_items = []
-        for subito_list_page_item in self.__subito_list_page_items:
-            subito_list_page_item["page"] = extracted_subito_list_page.subito_list_page.page_number
-            subito_list_page_item["timestamp"] = datetime.now()
-            subito_list_page_item["shipping_available"] = "spedizione disponibile" in subito_list_page_item["price"].lower()
-            if subito_list_page_item["price"]:
-                subito_list_page_item["price"] = subito_list_page_item["price"].split()[0].replace(".", "").replace(",", ".")
-            subito_list_page_item["identifier"] = subito_list_page_item["url"].split("-")[-1].split(".")[0]
-            subito_list_page_item["sold"] = bool(subito_list_page_item["sold"])
-            try:
-                subito_list_page_item["price"] = float(subito_list_page_item["price"])
-            except:
-                subito_list_page_item["price"] = None
-
+    def __init__(self, extracted_subito_list_page: ExtractedSubitoListPage, subito_list_page_items: list[dict]) -> None:
+        super().__init__(extracted_subito_list_page, extracted_subito_list_page.response_text)
+        self.__subito_list_page_items = subito_list_page_items
+        
     @property
     def subito_list_page_items(self):
         return self.__subito_list_page_items
     
-    @property
-    def extracted_subito_list_page(self):
-        return self.__extracted_subito_list_page
+    def __getitem__(self, page_number: int) -> SubitoListPage:
+        subito_list_page = super().__getitem__(page_number)
+        extracted_subito_list_page = extract_subito_list_page(subito_list_page)
+        try:
+            transformed_subito_list_page = transform_extracted_subito_list_page(extracted_subito_list_page)
+        except ValueError:
+            raise StopIteration
+        return transformed_subito_list_page
 
-    def __str__(self) -> str:
-        return f"({self.__class__.__name__}: {self.extracted_subito_list_page})"
-    
-    def __iter__(self):
-        return iter(self.subito_list_page_items)
-    
-    def __getitem__(self, index):
-        return self.subito_list_page_items[index]
-    
-    def __len__(self):
-        return len(self.subito_list_page_items)
-    
-    @classmethod
-    def from_subito_list_page(cls, subito_list_page: SubitoListPage):
-        '''
-        # Transformed Subito List Page > From Subito List Page
-        Create a TransformedSubitoListPage object from a SubitoListPage object.
+def extract_subito_list_page(subito_list_page: SubitoListPage) -> ExtractedSubitoListPage:
+    '''
+    # Extract Subito List Page
+    Extract a Subito List Page.
 
-        Arguments
-        ---------
-        `subito_list_page: SubitoListPage`
-            The SubitoListPage object to transform.
-        '''
-        return cls(ExtractedSubitoListPage(subito_list_page))
+    Arguments
+    ---------
+    `subito_list_page: SubitoListPage`
+        The SubitoListPage object to extract.
+    '''
+    response_text = simplified_get(subito_list_page.url)
+    return ExtractedSubitoListPage(subito_list_page, response_text)
 
-    @classmethod
-    def from_url(cls, url: str):
-        '''
-        # Transformed Subito List Page > From URL
-        Create a TransformedSubitoListPage object from an url.
+__subito_list_page_extractor = Extractor.from_yaml_file(f'{extractors_directory}/subito_list_page_extractor.yaml')
 
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
-        '''
-        return cls(ExtractedSubitoListPage.from_url(url))
+def transform_extracted_subito_list_page(extracted_subito_list_page: ExtractedSubitoListPage) -> TransformedSubitoListPage:
+    '''
+    # Transform Subito List Page
+    Transform an ExtractedSubitoListPage into a TransformedSubitoListPage.
 
-    @classmethod
-    def from_query(cls, query: str, page_number: int = 1):
-        '''
-        # Transformed Subito List Page > From Query
-        Create a TransformedSubitoListPage object from a query.
+    Arguments
+    ---------
+    `extracted_subito_list_page: ExtractedSubitoListPage`
+        The ExtractedSubitoListPage object to transform.
+    '''
+    response_text = extracted_subito_list_page.response_text
+    result = __subito_list_page_extractor.extract(response_text)
+    subito_list_page_items = result["subito_list_page_items"]
+    if not subito_list_page_items:
+        raise ValueError("The Subito List Page is empty.")
+    for subito_list_page_item in subito_list_page_items:
+        subito_list_page_item["page"] = extracted_subito_list_page.page_number
+        subito_list_page_item["timestamp"] = datetime.now()
+        subito_list_page_item["shipping_available"] = "spedizione disponibile" in subito_list_page_item["price"].lower()
+        if subito_list_page_item["price"]:
+            subito_list_page_item["price"] = subito_list_page_item["price"].split()[0].replace(".", "").replace(",", ".")
+        subito_list_page_item["identifier"] = subito_list_page_item["url"].split("-")[-1].split(".")[0]
+        subito_list_page_item["sold"] = bool(subito_list_page_item["sold"])
+        try:
+            subito_list_page_item["price"] = float(subito_list_page_item["price"])
+        except:
+            subito_list_page_item["price"] = None
+    return TransformedSubitoListPage(extracted_subito_list_page, subito_list_page_items)
 
-        Arguments
-        ---------
-        `query: str`
-            The query of the page.
-        '''
-        return cls(ExtractedSubitoListPage.from_query(query, page_number))
+def extract_and_transform_subito_list_page(subito_list_page: SubitoListPage) -> TransformedSubitoListPage:
+    '''
+    # Extract and Transform Subito List Page
+    Extract and transform a SubitoListPage into a TransformedSubitoListPage.
+
+    Arguments
+    ---------
+    `subito_list_page: SubitoListPage`
+        The SubitoListPage object to extract and transform.
+    '''
+    extracted_subito_list_page = extract_subito_list_page(subito_list_page)
+    return transform_extracted_subito_list_page(extracted_subito_list_page)
+
+def subito_list_page_item_iterator(subito_list_page: SubitoListPage) -> Iterator[dict]:
+    '''
+    # Subito List Page Item Iterator
+    Iterate over the items of a SubitoListPage.
+
+    Arguments
+    ---------
+    `subito_list_page: SubitoListPage`
+        The SubitoListPage object to iterate over.
+    '''
+    for transformed_subito_list_page in extract_and_transform_subito_list_page(subito_list_page):
+        for subito_list_page_item in transformed_subito_list_page.subito_list_page_items:
+            yield subito_list_page_item
+
