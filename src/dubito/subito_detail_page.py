07433fe12aaa6e2cd0ced7c694f08cded13a692a
daqh <valeriodipasq@gmail.com>
2023-05-28 23:48:11 +0200

    Update page

--- src/dubito/subito_detail_page.py
+++ src/dubito/subito_detail_page.py
@@ -4,31 +4,11 @@ from dubito.utils import simplified_get, extractors_directory
 import logging
 
 class SubitoDetailPage:
-    '''
-    # SubitoDetailPage
-    A SubitoDetailPage is a page that contains the details of an insertion.
-
-    Attributes
-    ----------
-    `url: str`
-        The url of the page.
-    '''
-
+    
     def __init__(self, url: str):
-        '''
-        # SubitoDetailPage > Constructor
-        The constructor for the SubitoDetailPage class.
-
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
-        `identifier: str`
-            The identifier of the page.
-        '''
         self.__url = url
         self._identifier = self.url.split("-")[-1].split(".")[0]
-
+    
     @property
     def url(self):
         return self.__url
@@ -36,131 +16,50 @@ class SubitoDetailPage:
     @property
     def identifier(self):
         return self._identifier
-
+    
     def __str__(self) -> str:
         return f"({self.__class__.__name__}: {self.identifier})"
-
-class ExtractedSubitoDetailPage:
-    '''
-    # ExtractedSubitoDetailPage
-    An ExtractedSubitoDetailPage is a page that contains the details of an insertion and its extracted data.
-
-    Attributes
-    ----------
-    `detail_page: SubitoDetailPage`
-        The SubitoDetailPage object to extract.
-    `response: str`
-        The text of the page.
-    '''
-
-    def __init__(self, detail_page: SubitoDetailPage):
-        '''
-        # ExtractedSubitoDetailPage > Constructor
-
-        Arguments
-        ---------
-        `detail_page: SubitoDetailPage`
-            The SubitoDetailPage object to extract.
-        '''
-        self.__detail_page = detail_page
-        logging.info(f"Extracting {self}")
-        response_text = simplified_get(detail_page.url)
-        self.__response = response_text
     
+class ExtractedSubitoDetailPage(SubitoDetailPage):
+
+    def __init__(self, subito_detail_page: SubitoDetailPage, response_text: str) -> None:
+        super().__init__(subito_detail_page.url)
+        self.__response = response_text
+
     @property
     def response(self):
         return self.__response
-    
-    @property
-    def detail_page(self):
-        return self.__detail_page
-    
-    def __str__(self) -> str:
-        return f"({self.__class__.__name__}: {self.detail_page})"
-    
-    @classmethod
-    def from_url(cls, url: str):
-        '''
-        # ExtractedSubitoDetailPage > From URL
-        Create an ExtractedSubitoDetailPage object from an url.
 
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
+class TransformedSubitoDetailPage(ExtractedSubitoDetailPage):
 
-        Example
-        -------
-        ```python
-        from subito import ExtractedSubitoDetailPage
-        extracted_detail_page = ExtractedSubitoDetailPage.from_url("https://www.subito.it/vi/180882013.htm")
-        ```
-        '''
-        return cls(SubitoDetailPage(url))
-
-class TransformedSubitoDetailPage:
-    '''
-    # TransformedSubitoDetailPage
-    A TransformedSubitoDetailPage is a page that contains the details of an insertion and its extracted and transformed data.
-
-    Attributes
-    ----------
-    `extracted_detail_page: ExtractedSubitoDetailPage`
-        The ExtractedSubitoDetailPage object to transform.
-    `subito_detail_page_item: dict`
-        The transformed data.
-    '''
-
-    __subito_detail_page_extractor = Extractor.from_yaml_file(f'{extractors_directory}/subito_detail_page_extractor.yaml')
-
-    def __init__(self, extracted_detail_page: ExtractedSubitoDetailPage):
-        '''
-        # TransformedSubitoDetailPage > Constructor
-        
-        Arguments
-        ---------
-        `extracted_detail_page: ExtractedSubitoDetailPage`
-            The ExtractedSubitoDetailPage object to transform.
-        '''
-        self.__extracted_detail_page = extracted_detail_page
-        self.__subito_detail_page_item = self.__subito_detail_page_extractor.extract(extracted_detail_page.response)
-        self.__subito_detail_page_item["timestamp"] = datetime.now()
-        self.__subito_detail_page_item["price"] = float(self.__subito_detail_page_item["price"].replace("€", "").replace(".", "").replace(",", "."))
-        self.__subito_detail_page_item["shipping_available"] = bool(self.__subito_detail_page_item["shipping_available"])
-        self.__subito_detail_page_item["sold"] = bool(self.__subito_detail_page_item["sold"])
-        self.__subito_detail_page_item["city"] = self.__subito_detail_page_item["location"].split()[0]
-        self.__subito_detail_page_item["state"] = self.__subito_detail_page_item["location"].split()[1]
-        self.__subito_detail_page_item["identifier"] = self.extracted_detail_page.detail_page.identifier
-        del self.__subito_detail_page_item["location"]
-    
-    @property
-    def extracted_detail_page(self):
-        return self.__extracted_detail_page
+    def __init__(self, extracted_subito_detail_page: ExtractedSubitoDetailPage, subito_detail_page_item: dict) -> None:
+        super().__init__(extracted_subito_detail_page, extracted_subito_detail_page.response)
+        self.__subito_detail_page_item = subito_detail_page_item
 
     @property
     def subito_detail_page_item(self):
         return self.__subito_detail_page_item
-    
-    def __str__(self) -> str:
-        return f"({self.__class__.__name__}: {self.extracted_detail_page})"
 
-    @classmethod
-    def from_url(cls, url: str):
-        '''
-        # TransformedSubitoDetailPage > From URL
-        Create a TransformedSubitoDetailPage object from an url.
+def extract_subito_detail_page(subito_detail_page: SubitoDetailPage) -> ExtractedSubitoDetailPage:
+    logging.info(f"Extracting {subito_detail_page}")
+    response_text = simplified_get(subito_detail_page.url)
+    return ExtractedSubitoDetailPage(subito_detail_page, response_text)
 
-        Arguments
-        ---------
-        `url: str`
-            The url of the page.
+__subito_detail_page_extractor = Extractor.from_yaml_file(f"{extractors_directory}/subito_detail_page.yml")
 
-        Example
-        -------
-        ```python
-        from subito import TransformedSubitoDetailPage
+def transform_subito_detail_page(extracted_subito_detail_page: ExtractedSubitoDetailPage) -> TransformedSubitoDetailPage:
+    subito_detail_page_item = __subito_detail_page_extractor.extract(extracted_subito_detail_page.response)
+    subito_detail_page_item["timestamp"] = datetime.now()
+    subito_detail_page_item["price"] = float(subito_detail_page_item["price"].replace("€", "").replace(".", "").replace(",", "."))
+    subito_detail_page_item["shipping_available"] = bool(subito_detail_page_item["shipping_available"])
+    subito_detail_page_item["sold"] = bool(subito_detail_page_item["sold"])
+    subito_detail_page_item["city"] = subito_detail_page_item["location"].split()[0]
+    subito_detail_page_item["state"] = subito_detail_page_item["location"].split()[1]
+    subito_detail_page_item["identifier"] = extracted_detail_page.detail_page.identifier
+    del subito_detail_page_item["location"]
+    return TransformedSubitoDetailPage(extracted_subito_detail_page, subito_detail_page_item)
 
-        TransformedSubitoDetailPage.from_url("https://www.subito.it/vi/180882013.htm")
-        '''
-        return cls(ExtractedSubitoDetailPage.from_url(url))
-    
+def extract_and_transform_subito_detail_page(subito_detail_page: SubitoDetailPage) -> TransformedSubitoDetailPage:
+    extracted_subito_detail_page = extract_subito_detail_page(subito_detail_page)
+    transformed_subito_detail_page = transform_subito_detail_page(extracted_subito_detail_page)
+    return transformed_subito_detail_page
