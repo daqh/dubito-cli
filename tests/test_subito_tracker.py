b3386b5e9c77e554bd6eb3dd5177126b53f713b3
daqh <valeriodipasq@gmail.com>
2023-05-29 17:30:02 +0200

    Add SubitoListPage tests

--- tests/test_subito_tracker.py
+++ tests/test_subito_tracker.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest import mock
-from dubito import subito_detail_page
-from dubito import subito_list_page
+from dubito import subito_detail_page, subito_list_page
 
 test_subito_list_page = open("tests/data/test_subito_list_page.html", "r").read()
 
@@ -9,108 +8,42 @@ class TestSubitoListPage(unittest.TestCase):
 
     def test_subito_list_page_without_page_number(self):
         url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070"
-        page = subito_list_page.SubitoListPage(url)
-        self.assertEqual(page.page_number, 1, "The page number should be 1 if not specified.")
+        list_page = subito_list_page.SubitoListPage(url)
+        self.assertEqual(list_page.url, url, "The url is not the same")
+        self.assertEqual(list_page.page_number, 1, "The page number is not the same")
+        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+        self.assertEqual(list_page[2].page_number, 2, "The page number is not the same")
 
     def test_subito_list_page_with_page_number(self):
         url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2"
-        page = subito_list_page.SubitoListPage(url)
-        self.assertEqual(page.page_number, 2, "The page number should be 2 if specified.")
+        list_page = subito_list_page.SubitoListPage(url)
+        self.assertEqual(list_page.url, url, "The url is not the same")
+        self.assertEqual(list_page.page_number, 2, "The page number is not the same")
 
     def test_subito_list_page_without_query(self):
-        url = "https://www.subito.it/annunci-italia/vendita/usato/?o=2"
-        with self.assertRaises(ValueError):
-            page = subito_list_page.SubitoListPage(url)
+        url = "https://www.subito.it/annunci-italia/vendita/usato/"
+        self.assertRaises(ValueError, subito_list_page.SubitoListPage, url)
 
-    def test_subito_list_page(self):
-        url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2"
-        page = subito_list_page.SubitoListPage(url)
-        self.assertEqual(page.url, url, "The url should be the same as the one passed to the constructor.")
-        self.assertEqual(page.query, "gtx 1070", "The query should be the same as the one passed to the constructor.")
+class TestSubitoListPageQuery(unittest.TestCase):
 
-    def test_subito_list_page_getitem(self):
-        page = subito_list_page.SubitoListPage("https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=1")
-        self.assertEqual(page[3].url, "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=3", "The url should be the same as the one passed to the constructor but with the new page.")
-        self.assertEqual(page[2].query, "gtx 1070", "The query should be the same as the one passed to the constructor.")
-        self.assertEqual(page[5].page_number, 5, "The page number should be 2 if specified.")
+    def test_subito_list_page_query_without_page_number(self):
+        list_page = subito_list_page.SubitoListPageQuery("gtx 1070")
+        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+        self.assertEqual(list_page.page_number, 1, "The page number is not the same")
 
-    def test_subito_query_list_page(self):
-        page = subito_list_page.SubitoQueryListPage("gtx 1070")
-        self.assertEqual(page.url, "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=1", "The url should be the same as the one passed to the constructor.")
-        self.assertEqual(page.query, "gtx 1070", "The query should be the same as the one passed to the constructor.")
-        self.assertEqual(page.page_number, 1, "The page number should be 1 if not specified.")
+    def test_subito_list_page_query_with_page_number(self):
+        list_page = subito_list_page.SubitoListPageQuery("gtx 1070", 2)
+        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+        self.assertEqual(list_page.page_number, 2, "The page number is not the same")
 
-class TestExtractedSubitoListPage(unittest.TestCase):
+    def test_subito_list_page_query_with_category(self):
+        list_page = subito_list_page.SubitoListPageQuery("gtx 1070", category="informatica")
+        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+        self.assertEqual(list_page.page_number, 1, "The page number is not the same")
+        self.assertEqual(list_page.url, "https://www.subito.it/annunci-italia/vendita/informatica/?q=gtx%201070&o=1", "The url is not the same")
 
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_list_page))
-    def test_extracted_subito_list_page(self, _):
-        url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2"
-        page = subito_list_page.SubitoListPage(url)
-        extracted_page = subito_list_page.ExtractedSubitoListPage(page)
-        self.assertEqual(extracted_page.response, test_subito_list_page, "The text should be the same as the one passed to the constructor.")
-
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_list_page))
-    def test_extracted_subito_list_from_url(self, _):
-        url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2"
-        extracted_page = subito_list_page.ExtractedSubitoListPage.from_url(url)
-        self.assertEqual(extracted_page.response, test_subito_list_page, "The text should be the same as the one passed to the constructor.")
-
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_list_page))
-    def test_extracted_subito_list_from_query(self, _):
-        query = "gtx 1070"
-        extracted_page = subito_list_page.ExtractedSubitoListPage.from_query(query)
-        self.assertEqual(extracted_page.response, test_subito_list_page, "The text should be the same as the one passed to the constructor.")
-
-class TestTransformedSubitoListPage(unittest.TestCase):
-
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_list_page))
-    def test_transformed_subito_list_page(self, _):
-        url = "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2"
-        page = subito_list_page.SubitoListPage(url)
-        extracted_page = subito_list_page.ExtractedSubitoListPage(page)
-        transformed_page = subito_list_page.TransformedSubitoListPage(extracted_page)
-        self.assertEqual(len(transformed_page.subito_list_page_items), 33, "The number of insertions should be 33.")
-        # Check that the keys are the same
-        keys = transformed_page.subito_list_page_items[0].keys()
-        self.assertTrue("title" in keys, "The insertion should have a title.")
-        self.assertTrue("price" in keys, "The insertion should have a price.")
-        self.assertTrue("thumbnail" in keys, "The insertion should have a price.")
-        self.assertTrue("url" in keys, "The insertion should have a location.")
-        self.assertTrue("city" in keys, "The insertion should have a date.")
-        self.assertTrue("sold" in keys, "The insertion should have a url.")
-        self.assertTrue("state" in keys, "The insertion should have a url.")
-        self.assertTrue("time" in keys, "The insertion should have a url.")
-        self.assertTrue("page" in keys, "The insertion should have a url.")
-        self.assertTrue("timestamp" in keys, "The insertion should have a url.")
-
-test_subito_detail_page = open("tests/data/test_subito_detail_page.html", "r").read()
-
-class TestSubitoDetailPage(unittest.TestCase):
-
-    def test_subito_detail_page(self):
-        url = "https://www.subito.it/informatica/pc-desktop-gaming-i7-8700k-rtx-2080-16gb-ram-ssd-500gb-roma-337616447.htm"
-        page = subito_detail_page.SubitoDetailPage(url)
-        self.assertEqual(page.url, url, "The url should be the same as the one passed to the constructor.")
-
-class TestExtractedSubitoDetailPage(unittest.TestCase):
-
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_detail_page))
-    def test_extracted_subito_detail_page(self, _):
-        url = "https://www.subito.it/informatica/pc-desktop-gaming-i7-8700k-rtx-2080-16gb-ram-ssd-500gb-roma-337616447.htm"
-        page = subito_detail_page.SubitoDetailPage(url)
-        extracted_page = subito_detail_page.ExtractedSubitoDetailPage(page)
-        self.assertEqual(extracted_page.response, test_subito_detail_page, "The text should be the same as the one passed to the constructor.")
-
-class TestTransformedSubitoDetailPage(unittest.TestCase):
-
-    @mock.patch("requests.get", return_value=mock.Mock(status_code=200, text=test_subito_detail_page))
-    def test_transformed_subito_detail_page(self, _):
-        url = "https://www.subito.it/informatica/pc-desktop-gaming-i7-8700k-rtx-2080-16gb-ram-ssd-500gb-roma-337616447.htm"
-        page = subito_detail_page.SubitoDetailPage(url)
-        extracted_page = subito_detail_page.ExtractedSubitoDetailPage(page)
-        transformed_page = subito_detail_page.TransformedSubitoDetailPage(extracted_page)
-        self.assertEqual(transformed_page.subito_detail_page_item["title"], "PC Gaming Ryzen 7 1700/GTX 1070 ti")
-        self.assertEqual(transformed_page.subito_detail_page_item["price"], 650)
-        self.assertEqual(transformed_page.subito_detail_page_item["sold"], False)
-        self.assertEqual(transformed_page.subito_detail_page_item["city"], "Ravenna")
-        self.assertEqual(transformed_page.subito_detail_page_item["state"], "(RA)")
+    def test_subito_list_page_query_with_page_number(self):
+        list_page = subito_list_page.SubitoListPageQuery("gtx 1070", category="informatica", page_number=2)
+        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+        self.assertEqual(list_page.page_number, 2, "The page number is not the same")
+        self.assertEqual(list_page.url, "https://www.subito.it/annunci-italia/vendita/informatica/?q=gtx%201070&o=2", "The url is not the same")
