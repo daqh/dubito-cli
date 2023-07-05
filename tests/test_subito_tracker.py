e9f1083c4206700ff21e824d312626e4c0fec425
daqh <valeriodipasq@gmail.com>
2023-07-05 08:54:54 +0200

    Update tests

--- tests/test_subito_tracker.py
+++ tests/test_subito_tracker.py
@@ -50,16 +50,25 @@ class TestSubitoListPageQuery(unittest.TestCase):
 
 class TestExtractedSubitoListPage(unittest.TestCase):
 
-    def test_extracted_subito_list_page_without_page_number(self):
-        list_page = subito_list_page.SubitoListPageQuery("gtx 1070")
-        list_page = subito_list_page.extract(list_page)
-        self.assertEqual(list_page.url, "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=1", "The url is not the same")
-        self.assertEqual(list_page.page_number, 1, "The page number is not the same")
-        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+    @mock.patch('requests.get', side_effect=lambda url: mock.Mock(text=test_subito_list_page, status_code=200))
+    def test_extracted_subito_list_page_from_url(self, _):
+        list_page = subito_list_page.SubitoListPage('https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070')
+        extracted_list_page = subito_list_page.extract_subito_list_page(list_page)
+        self.assertEqual(extracted_list_page.url, list_page.url, "The url is not the same")
+        self.assertEqual(extracted_list_page.page_number, list_page.page_number, "The page number is not the same")
 
-    def test_extracted_subito_list_page_with_page_number(self):
-        list_page = subito_list_page.SubitoListPageQuery("gtx 1070", 2)
-        list_page = subito_list_page.extract(list_page)
-        self.assertEqual(list_page.url, "https://www.subito.it/annunci-italia/vendita/usato/?q=gtx%201070&o=2", "The url is not the same")
-        self.assertEqual(list_page.page_number, 2, "The page number is not the same")
-        self.assertEqual(list_page.query, "gtx 1070", "The query is not the same")
+    @mock.patch('requests.get', side_effect=lambda url: mock.Mock(text=test_subito_list_page, status_code=200))
+    def test_extracted_subito_list_page_from_query_with_page(self, _):
+        list_page = subito_list_page.SubitoListPageQuery('gtx 1070', 2)
+        extracted_list_page = subito_list_page.extract_subito_list_page(list_page)
+        self.assertEqual(extracted_list_page.url, list_page.url, "The url is not the same")
+        self.assertEqual(extracted_list_page.page_number, list_page.page_number, "The page number is not the same")
+
+class TestTransformedSubitoListPage(unittest.TestCase):
+
+    @mock.patch('requests.get', side_effect=lambda url: mock.Mock(text=test_subito_list_page, status_code=200))
+    def test_transformed_subito_list_page_from_query(self, _):
+        list_page = subito_list_page.SubitoListPageQuery('gtx 1070')
+        transformed_list_page = subito_list_page.extract_and_transform_subito_list_page(list_page)
+        self.assertEqual(transformed_list_page.url, list_page.url, "The url is not the same")
+        self.assertEqual(transformed_list_page.page_number, list_page.page_number, "The page number is not the same")
